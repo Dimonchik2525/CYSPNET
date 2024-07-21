7cd599edaa81daf760a8bd9dcd0f18486e1277b2
Dima <dima.soldatenko26@gmail.com>
2024-07-21 17:27:47 +0400

    finished footer contacts block

--- src/components/Footer/footerSupport/FooterSupport.jsx
+++ src/components/Footer/footerSupport/FooterSupport.jsx
@@ -6,35 +6,41 @@ export const FooterSupport = () => {
          <div className="footerSupport__container">
             <div className="footerSupport__block">
                <h3 className="footerSupport__title">Поддержка</h3>
-               <div className="footerSupport__ideas">
-                  <div className="footerSupport__ideas__title">Предложения и идеи</div>
-                  <div className="footerSupport__ideas__subtitle">Вы можете написать нам на почту или в личные сообщения Вконтакте</div>
-                  <ul className="footerSupport__ideas__list">
-                     <li className="footerSupport__ideas__item">
-                        <h4 className="footerSupport__ideas__item__title">Вконтакте:</h4>
-                        <a href="#" className="footerSupport__ideas__item__link">vk.com/cyspnet</a>
-                     </li>
-                     <li className="footerSupport__ideas__item">
-                        <h4 className="footerSupport__ideas__item__title">Email:</h4>
-                        <a href="#" className="footerSupport__ideas__item__link">ceo@cyspnet.com</a>
-                     </li>
-                  </ul>
-               </div>
-               <div className="footerSupport__crowdfunding">
-                  <h3 className="footerSupport__crowdfunding__title">Краудфандинг</h3>
-                  <ul className="footerSupport__crowdfunding__list">
-                     <li className="footerSupport__crowdfunding__item">
-                        <a href="" className="footerSupport__crowdfunding__link">
-                           <SVGSupport1Icon />
-                        </a>
-                        <a href="" className="footerSupport__crowdfunding__link">
-                           <SVGSupport2Icon />
-                        </a>
-                        <a href="" className="footerSupport__crowdfunding__link">
-                           <SVGSupportVKIcon />
-                        </a>
-                     </li>
-                  </ul>
+               <div className="footerSupport__content">
+                  <div className="footerSupport__ideas">
+                     <div className="footerSupport__ideas__title">Предложения и идеи</div>
+                     <div className="footerSupport__ideas__subtitle">Вы можете написать нам на почту или в личные сообщения Вконтакте</div>
+                     <ul className="footerSupport__ideas__list">
+                        <li className="footerSupport__ideas__item">
+                           <h4 className="footerSupport__ideas__item__title">Вконтакте:</h4>
+                           <a href="#" className="footerSupport__ideas__item__link">vk.com/cyspnet</a>
+                        </li>
+                        <li className="footerSupport__ideas__item">
+                           <h4 className="footerSupport__ideas__item__title">Email:</h4>
+                           <a href="#" className="footerSupport__ideas__item__link">ceo@cyspnet.com</a>
+                        </li>
+                     </ul>
+                  </div>
+                  <div className="footerSupport__crowdfunding">
+                     <h3 className="footerSupport__crowdfunding__title">Краудфандинг</h3>
+                     <ul className="footerSupport__crowdfunding__list">
+                        <li className="footerSupport__crowdfunding__item">
+                           <a href="" className="footerSupport__crowdfunding__link">
+                              <SVGSupport1Icon />
+                           </a>
+                        </li>
+                        <li className="footerSupport__crowdfunding__item">
+                           <a href="" className="footerSupport__crowdfunding__link">
+                              <SVGSupport2Icon />
+                           </a>
+                        </li>
+                        <li className="footerSupport__crowdfunding__item">
+                           <a href="" className="footerSupport__crowdfunding__link">
+                              <SVGSupportVKIcon />
+                           </a>
+                        </li>
+                     </ul>
+                  </div>
                </div>
             </div>
          </div>
